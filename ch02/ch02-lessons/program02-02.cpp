ad719e84acb22f0eb65ae11721c23c3faba8780f
erikbloodshed <erik.bloodshed@hotmail.com>
2024-11-14 07:59:59 +0800

    new file:   ch02-lessons/program02-02.cpp

---
+++ ch02/ch02-lessons/program02-02.cpp
@@ -0,0 +1,17 @@
+/********************************************************************
+ * This program shows how we can print a square of asterisks.       *
+ ********************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Print a square of asterisks
+    cout << "******" << endl;
+    cout << "******" << endl;
+    cout << "******" << endl;
+    cout << "******" << endl;
+    cout << "******" << endl;
+    cout << "******";
+    return 0;
+}
